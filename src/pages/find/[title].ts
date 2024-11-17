46e1f91fcd753d9029df949b4d9d13a32644b6e3
ledionres <ledionrestelica@icloud.com>
2024-11-17 13:32:30 +0100

    Fix search

--- src/pages/find/[title].ts
+++ src/pages/find/[title].ts
@@ -6,13 +6,11 @@ export async function GET({ params }: { params: { title: string } }) {
   const { title } = params;
 
   try {
-    // Decode the title in case it's encoded in the request
     const decodedTitle = decodeURIComponent(title);
 
-    // Fetch data using GROQ
     const result = await client.fetch(
       `*[_type == "carCover" && title match $title]`,
-      { title: `"${decodedTitle}"` } // Enclose in quotes for exact matches with spaces
+      { title: `"${decodedTitle}"` }
     );
 
     if (!result.length) {
